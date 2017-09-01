fc8302298cc355a857b7a6b53d4b8506c502d941
Chris <chris@w520>
2017-09-01 10:51:03 -0400

    current install script.
    It has the problem of e2iamge might corrupt sparse file.
    The mkfs.ext4 has flags problem.

--- scripts/inst.sh
+++ scripts/inst.sh
@@ -1,59 +1,73 @@
 
 base=`dirname $0`
-dstdev=/dev/sda
+dstdev=/dev/sdb
 boot="$dstdev"1
 lvm="$dstdev"2
-vg=cl_archiso
-lvsize=25141248S
-swsize=3031040S
+vg=VolGroup
+lvsize=860160000S
+swsize=66076672S 
 bootarch=boot.tar.bz2
 rootarch=cl_archiso-root.tar.bz2
-
+bootqcow=sda1.qcow2
+rootqcow=VolGroup-lv_root.qcow2
+mbr=sda.mbr.dd
 
 
 echo base is $base
 
-vgchange -an $vg
+#vgchange -an $vg
+vgchange -an 
 
 # empty the GTP label
-dd if=/dev/zero of=$dstdev bs=1M count=2
+dd if=$mbr of=$dstdev
 
 parted -s $dstdev mklabel msdos
 parted  $dstdev <<EOF
-mkpart  primary ext4 2048s  2099199s
-mkpart	primary 2099200s  30277631s
+mkpart  primary ext4 2048s 1026047s 
+mkpart	primary 1026048s  1952448511s
 set 2 lvm on
 unit s
 p
 EOF
 
-mkfs.ext4 $boot
-e2label $boot /boot
+e2image -ra $bootqcow $boot
+#mkfs.ext4 $boot
+#e2label $boot /boot
 
 pvcreate -ff -y $lvm
 pvs --unit s
 vgcreate $vg $lvm
 vgs --unit s
-lvcreate  -y -L $lvsize -n root $vg
-lvcreate  -y -L $swsize -n swap $vg
+lvcreate  -y -L $lvsize -n lv_root $vg
+lvcreate  -y -L $swsize -n lv_swap $vg
+lvcreate  -y -L 127200000S -n lv_home $vg
+lvcreate  -y -L 184350464S -n lv_bglog $vg
 lvs --unit s
 
-mkswap /dev/mapper/$vg-swap
-mkfs.ext4 /dev/mapper/$vg-root
+e2image -ra $rootqcow /dev/mapper/$vg-lv_root
+mkswap /dev/mapper/$vg-lv_swap
+mkfs.ext4 /dev/mapper/$vg-lv_home
+mkfs.ext4 /dev/mapper/$vg-lv_bglog
+
+
+#mkswap /dev/mapper/$vg-lv_swap
+#mkfs.ext4 /dev/mapper/$vg-lv_root
 
 [ ! -d /img ] && mkdir /img
-mount /dev/mapper/$vg-root /img
+mount /dev/mapper/$vg-lv_root /img
 [ ! -d /img/boot ] && mkdir /img/boot
-mount $boot /img/boot
-tar jxvf $base/$rootarch -C /img
-tar jxvf $base/$bootarch -C /img/boot
+#tar jxvf $base/$rootarch -C /img
+#tar jxvf $base/$bootarch -C /img/boot
 
 mount -t proc proc /img/proc
 mount -o bind /dev /img/dev
 mount -t sysfs sys /img/sys
 mount -o bind /dev/pts /img/dev/pts
 
-chroot /img /usr/sbin/grub2-install --boot-directory=/boot $dstdev
+#chroot /img /usr/sbin/grub2-install --boot-directory=/boot $dstdev
+chroot /img/ /bin/env PATH=/bin:/usr/bin:/sbin:/usr/sbin /bin/mount $boot /boot
+cp device.map /img/boot/grub
+chroot /img  /bin/env PATH=/bin:/usr/bin:/sbin:/usr/sbin /sbin/grub-install $dstdev
 
 # need to update fstab due to uuid change
 
