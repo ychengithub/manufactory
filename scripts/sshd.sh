4baec2cf893976047fd25cacb10d893fbf24309a
Chris <chris@w520>
2017-09-15 23:51:33 -0400

    Update sshd.sh

--- scripts/sshd.sh
+++ scripts/sshd.sh
@@ -1,8 +1,9 @@
 base=`dirname 0`
-cp -a $base/keys/* /etc/ssh/
-chmod 600 /etc/ssh/*.pub
-ifconfig enp2s0f0 192.168.10.135 up
-systemctl start sshd.service
+cp $base/keys/* /etc/ssh/
+chmod 600 /etc/ssh/*key*
+#ifconfig enp2s0f0 192.168.10.135 up
+#systemctl start sshd.service
+service sshd restart
 [ ! -d "/root/.ssh" ] && mkdir /root/.ssh
 chmod 700 /root/.ssh
 cp $base/authorized_keys  /root/.ssh/
