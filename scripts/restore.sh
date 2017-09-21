0ea109d4fd55e3008b2a78d853bc7a1e747a0693
Chris <chris@w520>
2017-09-20 22:30:32 -0400

    fixup the install script for the golden partition

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -2,11 +2,11 @@
 base=`dirname $0`
 base=`realpath "$base"`
 
-dstdev=/dev/sdc
+#dstdev=/dev/sdc
 
 vgchange -ay VolGroup
 lvconvert --merge VolGroup/lv_root.factory
 lvcreate -s -L 10G -n lv_root.factory VolGroup/lv_root
-partclone.ext4 -c -b -s /dev/VolGroup/lv_boot.factory -O /dev/sdc1
+# partclone.ext4 -c -b -s /dev/VolGroup/lv_boot.factory -O /dev/sdc1
  
 
